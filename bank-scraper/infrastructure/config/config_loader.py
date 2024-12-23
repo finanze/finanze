f9cb1c6a2027c9d181fb850854a488a3094d1cb5
marcosav <marcosav2010@gmail.com>
2024-12-23 21:40:20 +0100

    feat: improve config write

--- bank-scraper/infrastructure/config/config_loader.py
+++ bank-scraper/infrastructure/config/config_loader.py
@@ -1,3 +1,6 @@
+import shutil
+from pathlib import Path
+
 import yaml
 from cachetools import TTLCache, cached
 
@@ -9,6 +12,7 @@ class ConfigLoader(ConfigPort):
 
     def __init__(self, path: str) -> None:
         self.config_file = path
+        self.config_path = Path(path)
 
     @cached(cache=TTLCache(maxsize=1, ttl=30))
     def load(self) -> dict:
@@ -16,17 +20,9 @@ class ConfigLoader(ConfigPort):
             return yaml.safe_load(file)
 
     def check_or_create_default_config(self):
-        try:
-            self.load()
-        except FileNotFoundError:
+        if not self.config_path.is_file():
             print(f"Config file not found, creating default config at {self.config_file}")
-            with open(self.DEFAULT_CONFIG_PATH, "r") as file:
-                with open(self.config_file, "w") as new_file:
-                    new_file.write(file.read())
-
-        except Exception as e:
-            print(f"Error loading config file: {e}")
-            exit(1)
+            shutil.copy(self.DEFAULT_CONFIG_PATH, self.config_file)
 
         self.load()
         print(f"Config file loaded from {self.config_file}")
