2336c33c9b762338ec169fa5bb94947f38a20ff5
marcosav <marcosav2010@gmail.com>
2025-04-04 18:34:05 +0200

    fix: sego tx duplication

--- bank-scraper/infrastructure/scrapers/sego/sego_scraper.py
+++ bank-scraper/infrastructure/scrapers/sego/sego_scraper.py
@@ -176,7 +176,7 @@ class SegoScraper(EntityScraper):
             types = []
 
         raw_movements = []
-        page = 0
+        page = 1
         while True:
             fetched_movs = copy.deepcopy(self._client.get_movements(page=page, limit=100))
             raw_movements += fetched_movs
